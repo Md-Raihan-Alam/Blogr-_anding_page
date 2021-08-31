7e82cdb592374b1aa9d9dd74665b2486bb3d0e6c
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2021-08-31 16:24:55 +0600

    icon toggle
    code for icon toggle has been completed

--- blogr-script.js
+++ blogr-script.js
@@ -1,3 +1,4 @@
+//nav bar 
 const btn=document.querySelectorAll('.menu_section_link_one');
 const dropdowns=document.querySelectorAll('.dropdown_menu');
 btn.forEach((e)=>{
@@ -13,4 +14,21 @@ btn.forEach((e)=>{
         });
         ele.style.visibility="visible";
     });
+});
+//nab bar
+
+//menu icon
+let changeIcon=true;
+const menuIconDom=document.querySelector('.menu_bar_icon');
+const menuIcon=document.querySelector('.menu_bar_icon i');
+menuIcon.addEventListener('click',()=>{
+    if(changeIcon){
+        menuIcon.classList.remove('fa-bars');
+        menuIcon.classList.add('fa-times');
+        changeIcon=false;
+    }else{
+       menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars');
+        changeIcon=true;
+    }
 });
